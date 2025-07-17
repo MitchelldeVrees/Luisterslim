dd6c22b499bf35a81e57b2d62084fd1a79c7a519
Mitchell de Vries <71980169+MitchelldeVrees@users.noreply.github.com>
2025-07-17 14:45:08 +0200

    Fix expo routing entry

--- App.tsx
+++ App.tsx
@@ -1,10 +1,2 @@
-export { default } from './app/index.tsx';
-// App.tsx (next to package.json)
-
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
-import 'react-native-gesture-handler';
-import 'react-native-get-random-values'; // only if you’re using any crypto in JS
-
-// THIS registers the router’s own root component for Expo to render
-registerRootComponent(ExpoRoot);
+// Entry point for Expo Router
+import 'expo-router/entry';
